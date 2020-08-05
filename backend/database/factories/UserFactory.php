dfca5118a5728ac262b74deace5207c4c3be905c
David Albuquerque <davidra_rj@hotmail.com>
2020-08-05 00:42:43 -0300

    #fixed users factory and seeder. +added basic user authentication.

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -19,9 +19,13 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'admin' => false,
+        'nicks' => $faker->name,
+        'photo' => $faker->name,
+        'gender'=>$faker->randomElement(['feminine', 'masculine', 'other']),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
