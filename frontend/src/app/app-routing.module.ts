6184bf9f7c0cfe99e597265655c0818153c92552
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2020-08-08 21:10:05 -0300

    + página e layout básico de login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
---
+++ frontend/src/app/pages/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  submitLogin(form) {
+    console.log(form);
+    console.log(form.value);
+    this.loginForm.reset();
+  }
+
+}
