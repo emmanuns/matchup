13b12616e441dedfdd45e7de53d7013cc42cd230
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2020-08-10 17:43:57 -0300

    + scroll infinito na home

--- frontend/src/app/tab1/tab1.page.ts
+++ frontend/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-tab1',
@@ -6,12 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  posts = [];
   post_ex = {
     tags: '#League of Legends',
     text: 'E aí pessoal, tava querendo jogar uma partidinha hoje mais tarde. Alguém topa?',
     user_photo: '../../assets/example_avatar.jpg',
     user_name: 'jcrodriguez'
   };
-  constructor() {}
+  constructor() {
+    this.getPosts(20);
+  }
+
+  getPosts(size: number) {
+    for (let i = 0; i < size; i++) {
+      this.posts.push({
+        user_photo: faker.internet.avatar(),
+        user_name: faker.internet.userName(),
+        tags: faker.lorem.words(),
+        text: faker.lorem.paragraph(),
+      });
+    }
+  }
+
+  loadPosts(event) {
+    setTimeout(() => {
+      this.getPosts(20);
+      event.target.complete();
+      if(this.posts.length === 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
 
 }
